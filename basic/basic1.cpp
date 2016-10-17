882bab335e442618523b8989ece8a951015a1c28
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2016-10-18 02:02:41 +0800

    List other useful functions.

--- basic/basic1.cpp
+++ basic/basic1.cpp
@@ -6,9 +6,22 @@ void reshape(GLsizei, GLsizei);
 int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	// Available modes:
+	//   GLUT_SINGLE, GLUT_DOUBLE
+	//   GLUT_RGBA, GLUT_RGB, GLUT_INDEX
+	//   GLUT_DEPTH, GLUT_STENCIL, GLUT_ACCUM
+	// glutInitWindowSize(500, 500);
+	// glutInitWindowPosition(100, 100);
 	glutCreateWindow("Basic 1");
 	glutDisplayFunc(display);
+	// glutPostRedisplay();
+	// glutSwapBuffers();
 	glutReshapeFunc(reshape);
+	// glutKeyboardFunc(callback);
+	// glutMouseFunc(callback);
+	// glutMotionFunc(callback);
+	// glutPassiveMotionFunc(callback);
+	// glutIdleFunc(callback);
 	glutMainLoop();
 	return 0;
 }
@@ -18,11 +31,16 @@ void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1, 1, 1);
 	glutSolidTeapot(1);
+	// glutWireTeapot(1);
+	// glutSolidSphere(1, 1, 1);
+	// glutSolidCube(1);
+	// glutSolidCone(1, 1, 1, 1);
+	// glutSolidTorus(1, 1, 1, 1);
 	glFlush();
 }
 
-void reshape(GLsizei w, GLsizei h) {
-	glViewport(0, 0, w, h);
+void reshape(GLsizei width, GLsizei height) {
+	glViewport(0, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glFrustum(-0.5, 0.5, -0.5, 0.5, 1, 20);
